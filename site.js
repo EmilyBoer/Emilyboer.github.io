b829eef1cdb6ae691be58309844c0449595d3742
Emily Boerner <Emily.boerner@gmail.com>
2017-04-16 23:56:08 -0700

    add scroll animation to nav

--- site.js
+++ site.js
@@ -1,8 +1,15 @@
 $(window).scroll(function() {
     if ($(this).scrollTop() > 200){ // Set position from top to add class
         $('.navbar').addClass("background");
+        $('.nav-transition').addClass("scroll-margin");
+       
+       
     } else {
         $('.navbar').removeClass("background");
+        $('.nav-transition').removeClass("scroll-margin");
+
     }
 });
+
+
 

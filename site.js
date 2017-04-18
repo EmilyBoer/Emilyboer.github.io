06395877e370116cce554ddff26fa0ca85836386
Emily Boerner <Emily.boerner@gmail.com>
2017-04-17 21:25:13 -0700

    Carousel updates

--- site.js
+++ site.js
@@ -11,5 +11,29 @@ $(window).scroll(function() {
     }
 });
 
+$(document).ready(function(){
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+     nav: true,
+     navText: ["<img src='chevron-left.png'>","<img src='chevron-right.png'>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+})
 
+});
